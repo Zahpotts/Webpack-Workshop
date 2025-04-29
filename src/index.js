d61f5103f3c33650d833b36408d52809a2e2065d
Isaiah <zahpotts123@gmail.com>
2025-04-29 13:04:28 -0400

    fixed some stuff

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+
+const sampleCompoent = <div>ItS WORKING</div>;
+ReactDOM.render(sampleCompoent, rootElement);
